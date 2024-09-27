b8fb5aaa43335f894e064768951ff71430f67fb4
emiliodeleon <emiliodeleon.contact@gmail.com>
2024-09-27 17:43:12 -0600

    Started project and disable dark mode

---
+++ app/home/page.tsx
@@ -0,0 +1,12 @@
+import Reac from "react";
+
+const HomePage: React.FC  = () => {
+
+  return (
+    <section className="home-page-container">
+        <h1>Hello World!</h1>
+    </section>
+  );
+};
+
+export default HomePage;
