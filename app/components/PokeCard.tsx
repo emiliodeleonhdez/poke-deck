df682bfb5f1f6895a9e05fbbd9097027621ece96
emiliodeleon <emiliodeleon.contact@gmail.com>
2024-09-28 11:20:38 -0600

    Add action property to pokecard

--- app/components/PokeCard.tsx
+++ app/components/PokeCard.tsx
@@ -1,23 +1,17 @@
 import React from "react";
 import { Pokemon } from "../interfaces/pokemon";
-import { useRouter } from "next/navigation";
 
 type PokeCardProps = {
   pokeInfo: Pokemon;
+  action?: () => void;
 };
 
-const PokeCard: React.FC<PokeCardProps> = ({ pokeInfo }) => {
-  const router = useRouter();
-
+const PokeCard: React.FC<PokeCardProps> = ({ pokeInfo, action }) => {
   const { image, name, pokemonId } = pokeInfo;
-
-  const handlePokemonDetail = () => {
-    router.push(`/pokemon-detail?id=${pokemonId}`);
-  };
 
   return (
     <div
-      onClick={handlePokemonDetail}
+      onClick={action}
       key={pokemonId}
       className="border-2 border-transparent hover:border-pokemon_strong_blue cursor-pointer bg-pokemon bg-opacity-70 hover:bg-opacity-60 w-52 h-60 m-3 rounded-md p-4 flex flex-col items-center justify-start"
     >
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import useListPokemon from "../hooks/useListPokemon";
 import PokeCard from "../components/PokeCard";
+import { useRouter } from "next/navigation";
 
 const HomePage: React.FC = () => {
   const { pokemonList, loading, error } = useListPokemon(
     "/pokemon?limit=20&offset=20"
   );
+  const handlePokemonDetail = (id: number) => {
+    router.push(`/pokemon-detail?id=${id}`);
+  };
+
+  const router = useRouter();
 
   return (
     <section className="home-page-container w-full flex flex-wrap items-center justify-center">
@@ -13,7 +19,11 @@ const HomePage: React.FC = () => {
         <>Loading Pokémon... Gotta Catch 'Em All!</>
       ) : (
         pokemonList.map((pokemon, index) => (
-          <PokeCard key={index} pokeInfo={pokemon} />
+          <PokeCard
+            key={index}
+            pokeInfo={pokemon}
+            action={() => handlePokemonDetail(pokemon.pokemonId)}
+          />
         ))
       )}
     </section>
