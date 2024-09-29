af692e0dcd31888e23470794e67f257f9dda75d5
emiliodeleon <emiliodeleon.contact@gmail.com>
2024-09-29 10:24:39 -0600

    Adding suspense to mange params loading according to next docs

--- app/pokemon-detail/page.tsx
+++ app/pokemon-detail/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 import usePokemonDetails from "../hooks/usePokemonDetails";
 import PokeCardDetail from "../components/PokeCardDetail";
 
@@ -11,16 +11,18 @@ const PokemonDetail: React.FC = () => {
   const { pokemonDetails, evolutionChain } = usePokemonDetails(search);
 
   return (
-    <section className="pokemon-detail-container flex justify-center">
-      {pokemonDetails && evolutionChain ? (
-        <PokeCardDetail
-          pokeInfo={pokemonDetails}
-          evolutionChain={evolutionChain}
-        />
-      ) : (
-        <p>Loading...</p>
-      )}
-    </section>
+    <Suspense fallback={<p>Loading...</p>}>
+      <section className="pokemon-detail-container flex justify-center">
+        {pokemonDetails && evolutionChain ? (
+          <PokeCardDetail
+            pokeInfo={pokemonDetails}
+            evolutionChain={evolutionChain}
+          />
+        ) : (
+          <p>Loading...</p>
+        )}
+      </section>
+    </Suspense>
   );
 };
 
